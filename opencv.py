6220a5b5ec1959bcbedc3af927eb31e4af9949c2
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-02-25 22:04:43 +0530

    made changes

--- opencv.py
+++ opencv.py
@@ -6,15 +6,14 @@ import cv2
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
-cap=cv2.VideoCapture(0)
-while cap.isOpened():
-    ret,frame=cap.read()
-    if not ret:
-        break
-    cv2.imshow("Camera",frame)
-    key=cv2.waitKey(1) & 0xFF
-    if key==ord('q') or key==27:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+# cap=cv2.VideoCapture(0)
+# while cap.isOpened():
+#     ret,frame=cap.read()
+#     if not ret:
+#         break
+#     cv2.imshow("Camera",frame)
+#     key=cv2.waitKey(1) & 0xFF
+#     if key==ord('q') or key==27:
+#         break
+# cap.release()
+# cv2.destroyAllWindows()
